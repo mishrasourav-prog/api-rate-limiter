74b73edffae7c04b98b939676a0e6b066b07e474
SOURAV <mishrasourav448@gmail.com>
2026-03-14 20:23:02 +0530

    MINOR CHANGE

--- src/rate-limiter/limiterService.js
+++ src/rate-limiter/limiterService.js
@@ -1,15 +1,18 @@
 import fs from 'fs';
 import path from 'path';
-import {redis} from '../plugins/redis.plugin';
+import redis from '../plugins/redis.plugin.js'
 
-const luaScript = fs.readFileSync(path.join(process.cwd(), 'src/rate-limiter/limiter.lua'), 'utf-8');
+const luaScript = fs.readFileSync(path.join(process.cwd(), 'src/rate-limiter/tokenbucket.lua'), 'utf-8');
 
 export async function LimiterService(key){
     const capacity = process.env.RATE_LIMITER_CAPACITY;
     const refillRate = process.env.RATE_LIMITER_REFILL_RATE;
     const now = Math.floor(Date.now() / 1000);
 
-    const result = await redis.eval(luaScript , 1 , key , now , capacity , refillRate);
+    const result = await redis.eval(luaScript , {
+                    keys: [key],
+                    arguments: [capacity, refillRate, now]
+                })
 
     return result === 1;
 }
