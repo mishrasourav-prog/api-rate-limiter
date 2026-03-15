2da2fdb990c2cefc6c2d3a4cfddcbe688436396d
SOURAV <mishrasourav448@gmail.com>
2026-03-15 09:24:37 +0530

    MINOR CHANGE

--- src/middlewares/rateLimiter.middleware.js
+++ src/middlewares/rateLimiter.middleware.js
@@ -1,12 +1,17 @@
 import { LimiterService } from "../rate-limiter/limiterService";
 
+let totalRequests = 0;
+let blockedrequests = 0;
+
 export async function rateLimiterMiddleware(req , res){
+    totalRequests++;
     const key = 'rate:${req.ip}';
 
     const allowed = await LimiterService(key);
 
     if(!allowed){
-       return reply
+       blockedrequests++;
+       return res
        .header('X-RateLimit-Limit', process.env.RATE_LIMIT_CAPACITY)
        .code(429)
        .send({error: 'Too Many Requests'});
